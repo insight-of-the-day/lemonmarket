9da4d13265f5569b04624ef90a27cf40f5ed3e1c
admin <100tvlha@gmail.com>
2023-06-19 17:38:45 +0900

    송이 0619 판매내역 검색 페이징 수정(+)/최근본상품 삭제 로직(jsp) 변경하기(+)

--- lemon/src/main/java/co/market/lemon/mypage/command/MemberDelete.java
+++ lemon/src/main/java/co/market/lemon/mypage/command/MemberDelete.java
@@ -8,22 +8,42 @@ import co.market.lemon.common.Command;
 import co.market.lemon.member.service.MemberService;
 import co.market.lemon.member.service.MemberVO;
 import co.market.lemon.member.serviceImpl.MemberServiceImpl;
+import co.market.lemon.product.service.ProductService;
+import co.market.lemon.product.service.ProductVO;
+import co.market.lemon.product.serviceImpl.ProductServiceImpl;
+import co.market.lemon.reply.service.ReplyService;
+import co.market.lemon.reply.service.ReplyVO;
+import co.market.lemon.reply.serviceImpl.ReplyServiceImpl;
 
 public class MemberDelete implements Command {
 
 	@Override
 	public String exec(HttpServletRequest request, HttpServletResponse response) {
-		MemberService ms = new MemberServiceImpl();
-		MemberVO vo = new MemberVO();
 		HttpSession session = request.getSession();
-
-		vo.setMemberId((String) session.getAttribute("id"));
-		int n = ms.memberDelete(vo);
+		
+		ProductService ps = new ProductServiceImpl();
+		ProductVO pvo = new ProductVO();
+		ReplyService rs = new ReplyServiceImpl();
+		ReplyVO rvo = new ReplyVO();
+		MemberService ms = new MemberServiceImpl();
+		MemberVO mvo = new MemberVO();
+		
+		
+		mvo.setMemberId((String) session.getAttribute("id"));		
+		pvo.setProductWriter((String) session.getAttribute("name"));
+		rvo.setReplyWriter((String) session.getAttribute("name"));
+		
+//		ps.productNameDelete(pvo);
+//		rs.replyNameDelete(rvo);
+		
+		int n = ms.memberDelete(mvo);
 		if(n != 0) {
 			request.setAttribute("message", "회원 탈퇴가 완료되었습니다.");
 		} else {
 			request.setAttribute("message", "회원 탈퇴를 실패하였습니다.");
 		}
+		
+
 		
 		session.invalidate();
 		return "member/memberMessage";
